5066a4584d6f260009945cf82425ccce6aec032c
phatmap114 <phattt1504@gmail.com>
2024-07-20 00:30:19 +0700

    add meta

--- gas-khanhvan-fe/src/pages/index.tsx
+++ gas-khanhvan-fe/src/pages/index.tsx
@@ -13,11 +13,24 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Gas Khánh Vân Quận 7</title>
-        <meta name='description'>Gas Quận 7. Gas Giá t</meta>
+        <title>Cửa Hàng Gas Khánh Vân Quận 7</title>
         <meta
           name='google-site-verification'
           content='1576YrYuw5N2zO2oH75ShLXhWDn9rbJAd8DSNBcQYIs'
+        />
+        <meta
+          property='og:title'
+          content='Đại lý phân phối gas các quận tại cửa hàng gas Khánh Vân'
+        />
+        <meta
+          property='og:description'
+          content='Cửa hàng Gas tại TP.HCM cùng đội ngũ chuyên nghiệp. Chúng tôi cung cấp dịch vụ giao Gas Khánh Vân và lắp đặt miễn phí. Các phụ kiện về Gas gọi là có.'
+        />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://gas-khanh-van.vercel.app/' />
+        <meta
+          property='og:image'
+          content='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV6JUe_TejYzBFTSUEVdiHsHzVqWOLE1fGXg&s'
         />
       </Head>
       <div className={styles.container}>
