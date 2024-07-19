63c762d33c266bbe533a8228387b8cc75bfb7f92
phatmap114 <phattt1504@gmail.com>
2024-07-20 00:10:34 +0700

    add google verify seo

--- gas-khanhvan-fe/src/pages/_document.tsx
+++ gas-khanhvan-fe/src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import {Html, Head, Main, NextScript} from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
+      <meta name='google-site-verification' content='1576YrYuw5N2zO2oH75ShLXhWDn9rbJAd8DSNBcQYIs' />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
