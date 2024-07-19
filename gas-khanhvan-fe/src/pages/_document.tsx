2617ab72cec87745b3fcd6a8d119de2e42402f89
phatmap114 <phattt1504@gmail.com>
2024-07-20 00:18:05 +0700

    add google verify seo

--- gas-khanhvan-fe/src/pages/_document.tsx
+++ gas-khanhvan-fe/src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head />
-      <meta name='google-site-verification' content='1576YrYuw5N2zO2oH75ShLXhWDn9rbJAd8DSNBcQYIs' />
       <body>
         <Main />
         <NextScript />
--- gas-khanhvan-fe/src/pages/index.tsx
+++ gas-khanhvan-fe/src/pages/index.tsx
@@ -7,20 +7,31 @@ import Footer from '@/components/common/Footer'
 
 import styles from '../styles/home/Home.module.scss'
 import ProductSection from '@/components/home/ProductSection'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <HeadTag />
-      <Navbar />
-      <PromotionCarousel />
-      <main>
-        <div className={styles.top_bg}>
-          <Incentives />
-          <ProductSection />
-        </div>
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Gas Khánh Vân Quận 7</title>
+        <meta name='description'>Gas Quận 7. Gas Giá t</meta>
+        <meta
+          name='google-site-verification'
+          content='1576YrYuw5N2zO2oH75ShLXhWDn9rbJAd8DSNBcQYIs'
+        />
+      </Head>
+      <div className={styles.container}>
+        <HeadTag />
+        <Navbar />
+        <PromotionCarousel />
+        <main>
+          <div className={styles.top_bg}>
+            <Incentives />
+            <ProductSection />
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
   )
 }
